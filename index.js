343b7831fbde213c1fef9f855a4b618e20362b52
edgaresdras <36061735+edgaresdras@users.noreply.github.com>
2019-04-18 02:02:41 -0500

    Se agrego middleware

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require("express");
+const expressLayouts = require('express-ejs-layouts');
+const path = require('path');
 const router = require('./routes');
 
-require("dotenv").config({
-     path: "variables.env"
+require('dotenv').config({
+     path: 'variables.env'
 });
 
 const app = express();
 
-//Routing 
+// Habilitar EJS como template engine
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+//Ubicación vistas
+app.set('views', path.join(__dirname, './views'));
+
+//Archivos staticos
+app.use(express.static('public'));
+
+//Middleware (usuario logueado,flash messages, fecha actual)
+app.use((req, res, next) => {
+     const fecha = new Date();
+     res.locals.year = fecha.getFullYear();
+});
+
+//Routing
 app.use('/', router());
 
 //Agrega el puerto
